3f79195716f0690619f7def52372dacc38fe1141
Anandha Krishnan A <45736365+anandhakrishnanaji@users.noreply.github.com>
2023-12-07 08:12:06 +0530

    Add part 1 solution to day 4 challenge

--- 2023/day4/solution1.py
+++ 2023/day4/solution1.py
@@ -1,9 +1,16 @@
 import re
 
-REGEX_PATTERN = r"^Card \d+: ((\d+\s+)+)\|((\s+\d+)+)$"
+REGEX_PATTERN = r"^Card\s+\d+:\s+((\d+\s+)+)\|((\s+\d+)+)$"
 
 input_file = open("input.txt", "r")
 data = input_file.read()
 
-for i in re.finditer(REGEX_PATTERN, data, re.MULTILINE):
-    print(i)
+sum = 0
+
+for match in re.finditer(REGEX_PATTERN, data, re.MULTILINE):
+    winning_set = set([int(x) for x in match.group(1).split()])
+    playing_set = set([int(x) for x in match.group(3).split()])
+
+    sum += int(2 ** (len(winning_set.intersection(playing_set)) - 1))
+
+print(sum)
